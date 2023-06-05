46416c572d35dc3ffb60186c859a4cd4574ce28d
Ethan <ethan.j.cha@gmail.com>
2023-06-04 20:58:51 -0400

    worked on movement(incomplete)

--- Doors 2d/Player.gd
+++ Doors 2d/Player.gd
@@ -2,26 +2,42 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-var motion = Vector2()
+
+var motion : Vector2 = Vector2()
+var direction : Vector2 = Vector2()
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-
-
+func check_input():
+	motion = Vector2()
+	var moving = true
+	if Input.is_action_just_pressed("forward"):
+		motion.y -= 1
+		direction = Vector2(0, -1)
+#		$AnimatedSprite2D.play("forward")
+	elif Input.is_action_just_pressed("backward"):
+		motion.y += 1
+		direction = Vector2(0, 1)
+#	else:
+#		motion.y = 0
+#		moving = false or moving
+		
+	elif Input.is_action_just_pressed("left"):
+		motion.x -= 1
+		direction = Vector2(-1, 0)
+	elif Input.is_action_just_pressed("right"):
+		motion.x += 1
+		direction = Vector2(1, 0)
+#	else:
+#		motion.x = 0
+#		moving = false or moving
+	motion = motion.normalized()
+	motion = move_and_slide(motion*200)
 func _physics_process(delta):
 	# movement
-	
-	if Input.is_action_just_pressed("forward"):
-		motion.y = 20
-	elif Input.is_action_just_pressed("backward"):
-		motion.y = -20
-	else:
-		motion.y = 0
-	if Input.is_action_just_pressed("left"):
-		motion.x = -20
-	elif Input.is_action_just_pressed("right"):
-		motion.x = 20
-	else:
-		motion.x = 0
+	check_input()
+	$AnimatedSprite2D.play("forward")
+	#if moving == false:
+		#$AnimatedSprite2D.play("default")
 
 
 	# Get the input direction and handle the movement/deceleration.
