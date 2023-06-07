8e1fcd8eef2ea530452b155e4bb3ed3071e1e18b
Ethan <ethan.j.cha@gmail.com>
2023-06-07 08:39:25 -0400

    started actionable

--- Doors 2d/scripts/player.gd
+++ Doors 2d/scripts/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var speed = 400  # speed in pixels/sec
 
+
+
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
