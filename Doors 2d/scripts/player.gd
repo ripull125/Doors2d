3c7ad9fcbd1e6b83108c0ee83366737e28791d17
abratu2226 <allstar555700@gmail.com>
2023-06-08 13:24:52 -0400

    added closet

--- Doors 2d/scripts/player.gd
+++ Doors 2d/scripts/player.gd
@@ -2,10 +2,13 @@ extends CharacterBody2D
 
 var speed = 400  # speed in pixels/sec
 
-
+var coins = 0
+var key = 0
+var flashlight = 0
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 
 	move_and_slide()
+
