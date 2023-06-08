4a99e8f1424b0fdfabf2c149ed06a53114b32b44
Ethan <ethan.j.cha@gmail.com>
2023-06-08 13:18:32 -0400

    changes to player
    added down animation, organized files

--- Doors 2d/scripts/player.gd
+++ Doors 2d/scripts/player.gd
@@ -4,8 +4,13 @@ var speed = 400  # speed in pixels/sec
 
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
+	if direction.y > 0:
+		$AnimatedSprite2D.play("down")
+	else:
+		$AnimatedSprite2D.play("idle")
 	velocity = direction * speed
+	print(velocity)
 
 	move_and_slide()
