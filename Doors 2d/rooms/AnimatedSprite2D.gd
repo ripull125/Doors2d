c14b8347e3c490ec5a898c89c28746372da743e1
abratu2226 <allstar555700@gmail.com>
2023-06-08 12:44:00 -0400

    tried to make some movement stuff

---
+++ Doors 2d/rooms/AnimatedSprite2D.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta, String direction):
+	if direction == "left":
+		 
