6254fe4283926852b3d87d301e491dae58699d83
Ethan <ethan.j.cha@gmail.com>
2023-06-11 16:52:19 -0400

    closet changes

--- Doors 2d/scripts/closet.gd
+++ Doors 2d/scripts/closet.gd
@@ -8,8 +8,3 @@ class_name Interactable extends Area2D
  
 
 
-func _on_area_entered(area):
-	if (area.canHide && Input.is_action_just_pressed("use")):
-		$AnimatedSprite2D.play("open and close")
-	else:
-		$AnimatedSprite2D.play("idle")
