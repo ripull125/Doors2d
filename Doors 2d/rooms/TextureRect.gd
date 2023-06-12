a0afe3eb9646ef3799b16ef4eafc06c4fcd82a74
abratu2226 <allstar555700@gmail.com>
2023-06-12 14:56:08 -0400

    some hotbar stuff (it hard)

---
+++ Doors 2d/rooms/TextureRect.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const SlotClass = preload("res://Slot.gd")
+const ItemClass = preload("res://Item.gd")
+const NUM_INVENTORY_SLOTS = 20
+
+var inventory = (
+)
+# Called when the node enters the scene tree for the first time.
+func add_item(item_name, item_quantity):
+		for item in inventory:
+			if inventory[item][0] == item_name:
+				var stack_size = int(JsonData.item_data[item_name])
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
