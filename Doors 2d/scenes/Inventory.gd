5dd0222a2831cd959093ee788d83fdea3de39038
abratu2226 <allstar555700@gmail.com>
2023-06-13 17:25:22 -0400

    more hotbar stuff

--- Doors 2d/scenes/Inventory.gd
+++ Doors 2d/scenes/Inventory.gd
@@ -1,16 +1,33 @@
-@tool
-
 extends Node2D
 
 const SlotClass = preload("res://scenes/Slot.gd")
-onready var inventory_slots = $GridContainer
-var holding
+@onready var inventory_slots = $GridContainer
+var holding_item = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for inv_slot in inventory_slots.get_children():
+		inv_slot.connect(self, "gui_input", "slot_gui_input", [inv_slot])
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func slot_gut_input(event: InputEvent, slot: SlotClass):
+	if event is InputEvenMouseButton:
+		if event.button_index == BUTTON_LEFT && event.pressed:
+			if holding_item != null:
+				if !slot.item: # Placing holding item to slot
+					slot.putIntoSlot(holding_item)
+					holding_item = null
+				else: # Swap holding item with item in slot
+					var temp_item = slot.item
+					slot.pickFromSlot()
+					temp_item.global_position = event.global_position
+					slot.putIntoSlot(holding_item)
+					holding_item = temp_item
+			elif slot.item:
+				holding_item = slot.item
+				slot.pickFromSlot()
+				holding_item.global_position = get_global_mouse_position()
+				
+func _input(event):
+	if holding_item:
+		holding_item.global_position = get_global_mouse_position()
--- Doors 2d/scenes/Item.gd
+++ Doors 2d/scenes/Item.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if randi()%2 == 0:
+	if randi()%3 == 0:
 		$TextureRect.texture = load("res://key.png")
 	else:
 		$TextureRect.texture = load("res://tst.png")
