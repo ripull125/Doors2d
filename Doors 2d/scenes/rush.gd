4216614113d8ada5a748e4da56aeefd783bcfb95
Ethan <ethan.j.cha@gmail.com>
2023-06-15 13:18:32 -0400

    Update rush.gd

--- Doors 2d/scenes/rush.gd
+++ Doors 2d/scenes/rush.gd
@@ -6,7 +6,7 @@ var can_kill =true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$flicker.visible = false
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
