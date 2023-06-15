ed52aa9fda0093c6ff6ec7030c028afd63b2fe3d
Ethan <ethan.j.cha@gmail.com>
2023-06-15 13:15:07 -0400

    delete flicker

--- Doors 2d/scenes/rush.gd
+++ Doors 2d/scenes/rush.gd
@@ -14,9 +14,6 @@ func _process(_delta):
 	var my_random_number = rng.randf_range(-20000000.0, 20000000.0)
 	if(my_random_number >0 and my_random_number < 100000 and can_kill):
 		can_kill = false
-		$flicker.visible = true
-		await get_tree().create_timer(2.5).timeout
-		$flicker.visible = false
 		await get_tree().create_timer(5.0).timeout
 		kill = true
 		
