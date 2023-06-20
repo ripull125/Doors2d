5e8f342d41bba471d2c0280547c6bd2ad623c3cb
dean1210e <deanelbayar@gmail.com>
2023-06-20 08:44:59 -0400

    revert errors

--- Doors 2d/rooms/door.gd
+++ Doors 2d/rooms/door.gd
@@ -2,10 +2,8 @@ extends Area2D
 
 @onready var interact_label = $Interaction_Components/Interact_Label
 
-get_node(player).get(key)
-get_node(player).get(papers)
 
 func _on_body_entered(body):
-	if(key == 0 or papers < 5):
-		interact_label = "Make sure you have a key and five papers!"
+	#if(key == 0 or papers < 5):
+		#interact_label = "Make sure you have a key and five papers!"
 	get_tree().change_scene_to_file("res://rooms/REALroom_2.tscn")
