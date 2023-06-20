868cf7aa0f5597a838323d138f3eaa6974b559ad
abratu2226 <allstar555700@gmail.com>
2023-06-20 09:49:10 -0400

    final commit for door stuff

--- Doors 2d/rooms/door.gd
+++ Doors 2d/rooms/door.gd
@@ -1,9 +1,11 @@
+#class_name Interactable 
 extends Area2D
 
-@onready var interact_label = $Interaction_Components/Interact_Label
+@export var interact_label = "You need a key and all 5 papers!"
+@export var interact_type = "door"
+@export var interact_value = ""
 
 
-func _on_body_entered(body):
-	#if(key == 0 or papers < 5):
-		#interact_label = "Make sure you have a key and five papers!"
-	get_tree().change_scene_to_file("res://rooms/REALroom_2.tscn")
+ 
+
+
