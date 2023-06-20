ef37107a49aa0eb38f2316bfa5510fa70a7a7411
dean1210e <deanelbayar@gmail.com>
2023-06-19 20:09:21 -0400

    made/fixed doors

--- Doors 2d/rooms/Area2D.gd
+++ Doors 2d/rooms/Area2D.gd
@@ -3,7 +3,8 @@ extends Area2D
 @export var interact_type = "chaseMonster"
 @export var speed = 150
 
+
 func _process(delta):
-	get_parent().set_progress(get_parent().get_progress() + speed*delta)
-	if (get_parent().get_progress_ratio() ==1):
-		queue_free()
+	#get_parent().set_progress(get_parent().get_progress() + speed*delta)
+	#if (get_parent().get_progress_ratio() ==1):
+	#	queue_free()
