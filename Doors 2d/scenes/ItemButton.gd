ff8d7812456734cea838b767d7ee2744fb67298e
dean1210e <deanelbayar@gmail.com>
2023-06-20 08:46:39 -0400

    went back

--- Doors 2d/scenes/ItemButton.gd
+++ Doors 2d/scenes/ItemButton.gd
@@ -4,18 +4,17 @@ extends Button
 
 class_name ItemButton1
 
-#@export var item : Item :
-#	set(item_to_slot):
-#		item = item_to_slot
-#		icon = item.texture
-
-#var hand_equip : HandEquip
+@export var item : Item :
+	set(item_to_slot):
+		item = item_to_slot
+		icon = item.texture
+		
+var hand_equip : HandEquip
 
 func _ready():
 	connect("pressed", _on_pressed)
-
+	
 func _on_pressed():
-	pass
-#	if(item is EquipableItem):
-#		if(hand_equip != null):
-#			hand_equip.equipped_item = item
+	if(item is EquipableItem):
+		if(hand_equip != null):
+			hand_equip.equipped_item = item
